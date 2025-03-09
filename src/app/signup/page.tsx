a4c37ae3e0b9f506d39015d112c74bee5584ac79
Sharafat Hussain <sharafat.hussain9dec@gmail.com>
2025-03-09 08:00:33 -0700

    final comment

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -78,13 +78,13 @@ export default function SignUP() {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="h-4 w-4 opacity-70 text-slate-100"
+            className="h-4 w-4 opacity-70 text-slate-800"
           >
             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
           <input
-            className="text-slate-100 "
+            className="text-slate-800 "
             type="email"
             id="signUp-email"
             placeholder="Email"
@@ -100,13 +100,13 @@ export default function SignUP() {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="h-4 w-4 opacity-70 text-slate-100"
+            className="h-4 w-4 opacity-70 text-slate-800"
           >
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
           <input
             type="text"
-            className="UserName text-slate-100"
+            className="UserName text-slate-800"
             id="UserName"
             placeholder=" UserName"
             value={UserName}
@@ -121,7 +121,7 @@ export default function SignUP() {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="h-4 w-4 opacity-70 text-slate-100"
+            className="h-4 w-4 opacity-70 text-slate-800"
           >
             <path
               fillRule="evenodd"
@@ -130,7 +130,7 @@ export default function SignUP() {
             />
           </svg>
           <input
-            className="text-slate-100 "
+            className="text-slate-800 "
             type="password"
             id="signUp-password"
             placeholder=" Enter password"
@@ -146,7 +146,7 @@ export default function SignUP() {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="h-4 w-4 opacity-70 text-slate-100"
+            className="h-4 w-4 opacity-70 text-slate-800"
           >
             <path
               fillRule="evenodd"
@@ -155,7 +155,7 @@ export default function SignUP() {
             />
           </svg>
           <input
-            className="text-slate-100"
+            className="text-slate-800"
             type="password"
             id="ConfirmPassw"
             placeholder=" Confirm password"
--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,61 +1,126 @@
+// "use client";
+// import { Bar } from "react-chartjs-2";
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
+// import { UseFinanceContext } from "../../context/finaceContext";
+// import { ExpenseType } from "@/type/expenseType";
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+// export default function BarChart() {
+//   const { expense } = UseFinanceContext();
+
+//   const data = {
+//     labels: expense.map((items: ExpenseType) => items.title),
+
+//     datasets: [
+//       {
+//         label: "Expenses",
+//         data: expense.map((items: ExpenseType) => items.amount),
+//         backgroundColor: "rgba(255, 99, 132, 0.2)",
+//         borderColor: "rgb(255, 99, 132)",
+//         borderWidth: 1,
+//       },
+//     ],
+//   };
+
+//   const options = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         display: true, // Change from string to boolean
+//       },
+//       title: {
+//         display: true,
+//         text: "Your Chart Title", // Make sure this is a string
+//       },
+//     },
+//   };
+//   return (
+//     <div className="h-screen w-screen flex justify-center items-center  bg-slate-50">
+//       <div className=" fixed stats-parent w-screen flex justify-center items-center flex-col">
+//         <h1 className="text-xl font-bold">Expense Chart</h1>
+//         <div className=" char-child h-[300px] w-full flex justify-center items-center ">
+//           <Bar data={data} options={options} />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 "use client";
-import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
 import { UseFinanceContext } from "../../context/finaceContext";
 import { ExpenseType } from "@/type/expenseType";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+// Registering necessary chart elements for Pie chart
+ChartJS.register(ArcElement, Tooltip, Legend, Title);
 
-export default function BarChart() {
+export default function PieChart() {
   const { expense } = UseFinanceContext();
 
+  // Pie chart data
   const data = {
     labels: expense.map((items: ExpenseType) => items.title),
-
     datasets: [
       {
         label: "Expenses",
         data: expense.map((items: ExpenseType) => items.amount),
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ], // You can add more colors based on the number of expenses
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 206, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(153, 102, 255)",
+          "rgb(255, 159, 64)",
+        ], // Corresponding border colors
         borderWidth: 1,
       },
     ],
   };
 
+  // Pie chart options
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        display: true, // Change from string to boolean
+        display: true, // Show legend
       },
       title: {
         display: true,
-        text: "Your Chart Title", // Make sure this is a string
+        text: "Your Expense Distribution", // Title of the pie chart
       },
     },
   };
+
   return (
-    <div className="h-screen w-screen flex justify-center items-center  bg-slate-50">
-      <div className=" fixed stats-parent w-screen flex justify-center items-center flex-col">
+    <div className="h-screen w-screen flex justify-center items-center bg-slate-50">
+      <div className="fixed stats-parent w-screen flex justify-center items-center flex-col">
         <h1 className="text-xl font-bold">Expense Chart</h1>
-        <div className=" char-child h-[300px] w-full flex justify-center items-center ">
-          <Bar data={data} options={options} />
+        <div className="char-child h-[300px] w-full flex justify-center items-center">
+          <Pie data={data} options={options} />
         </div>
       </div>
     </div>
